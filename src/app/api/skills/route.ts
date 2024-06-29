39989e280df256d1a8e67348f06a9034a2bf2f90
Álvaro Nascimento <alvaro_4@live.com>
2024-06-29 18:51:26 -0300

    fix: remove db module of skills/route.ts

--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 import { prismaClient } from "../database/prismaClient";
-import db from "../database/dbConfig";
 
 const projects = [
    {
